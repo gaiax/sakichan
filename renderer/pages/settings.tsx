ed6cbd39f8afbd27ae9d603b01fbb5678e898c53
最上土川 <atsuyatsuchikawa@gmail.com>
2022-04-29 18:19:14 +0900

    tabの追加

--- renderer/pages/settings.tsx
+++ renderer/pages/settings.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import Layout from "../components/Layout";
 import { WindowSize } from "../interfaces";
+import { Container, Row, Col, Button, Tabs, Tab } from "react-bootstrap";
 
 const SettingPage = () => {
   useEffect(() => {
@@ -9,7 +10,23 @@ const SettingPage = () => {
   }, []);
   return (
     <Layout title="設定">
-      <h1>設定</h1>
+      <Container>
+        <Row>
+          <Col xs={3}>
+            <Button>戻る</Button>
+          </Col>
+          <Col>
+            <h2>設定</h2>
+          </Col>
+        </Row>
+        <Row className="mt-3">
+          <Tabs defaultActiveKey="teikei">
+            <Tab eventKey="teikei" title="定型文"></Tab>
+            <Tab eventKey="character" title="キャラクター"></Tab>
+            <Tab eventKey="dictionary" title="ユーザー辞書"></Tab>
+          </Tabs>
+        </Row>
+      </Container>
     </Layout>
   );
 };
