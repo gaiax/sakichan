a5530fb4767f7872fb3243061054b5d8de9fb6f3
最上土川 <atsuyatsuchikawa@gmail.com>
2022-04-29 16:44:19 +0900

    messageを返す処理を削除

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -39,6 +39,6 @@ app.on("window-all-closed", app.quit);
 
 // listen the channel `message` and resend the received message to the renderer process
 ipcMain.on("message", (event: IpcMainEvent, message: any) => {
+  console.log(event);
   console.log(message);
-  setTimeout(() => event.sender.send("message", "hi from electron"), 500);
 });
