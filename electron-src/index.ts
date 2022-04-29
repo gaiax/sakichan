593d10b1b6517198b39fcc67ec99593a4a282e01
最上土川 <atsuyatsuchikawa@gmail.com>
2022-04-29 17:46:37 +0900

    format

--- electron-src/index.ts
+++ electron-src/index.ts
@@ -30,10 +30,10 @@ app.on("ready", async () => {
   const url = isDev
     ? "http://localhost:8000/"
     : format({
-      pathname: join(__dirname, "../renderer/out/index.html"),
-      protocol: "file:",
-      slashes: true,
-    });
+        pathname: join(__dirname, "../renderer/out/index.html"),
+        protocol: "file:",
+        slashes: true,
+      });
 
   mainWindow.loadURL(url);
 });
@@ -42,37 +42,43 @@ app.on("ready", async () => {
 app.on("window-all-closed", app.quit);
 
 // listen the channel `message` and resend the received message to the renderer process
-ipcMain.on('message', (event: IpcMainEvent, message: any) => {
+ipcMain.on("message", (event: IpcMainEvent, message: any) => {
   console.log(event);
   console.log(message);
 
-  axios.post('http://localhost:50021/audio_query', {}, {
-    params: {
-      speaker: 9,
-      text: message
-    }
-  })
-    .then(function (res: AxiosResponse) {
-      console.log(res.data);
-      axios.post('http://localhost:50021/synthesis', res.data, {
+  axios
+    .post(
+      "http://localhost:50021/audio_query",
+      {},
+      {
         params: {
           speaker: 9,
-          enable_interrogative_upspeak: "yes",
+          text: message,
         },
-        responseType: 'arraybuffer'
-      })
+      }
+    )
+    .then(function (res: AxiosResponse) {
+      console.log(res.data);
+      axios
+        .post("http://localhost:50021/synthesis", res.data, {
+          params: {
+            speaker: 9,
+            enable_interrogative_upspeak: "yes",
+          },
+          responseType: "arraybuffer",
+        })
         .then(function (res: AxiosResponse) {
-          console.log(res.data.length);// res.dataが音声ファイルの中身
-          fs.writeFileSync(`./sound.wav`, Buffer.from(res.data), 'binary');
+          console.log(res.data.length); // res.dataが音声ファイルの中身
+          fs.writeFileSync(`./sound.wav`, Buffer.from(res.data), "binary");
           playWav(`./sound.wav`);
         })
         .catch((e: AxiosError<{ error: string }>) => {
           // エラー処理
           console.log(e.message);
-        })
+        });
     })
     .catch((e: AxiosError<{ error: string }>) => {
       // エラー処理
       console.log(e.message);
-    })
-})
+    });
+});
